16cc2e67136b0163e34505c85cb9c57883670c40
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-04-15 14:00:21 -0700

    including cookies with axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 // import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLDivElement
+  document.getElementById("root") as HTMLDivElement
 );
 root.render(
   <React.StrictMode>
